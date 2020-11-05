38e2c37a6f80011abca22651a98453224e178a04
v i t o r m u g n o l <cc19200@g.unicamp.br>
2020-11-05 23:56:37 +0000

    conversão de dez para outra funcional, e verificação

--- main.c
+++ main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <math.h>
 
 #define MAX 30
-
-char caracteres[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
 
 char antesVirgula[MAX], depoisVirgula[MAX];
 
@@ -32,21 +31,6 @@ void separarPorVirgula(char numero[MAX])
   }
 }
 
-/*char getLetra(int numero)
-{
-  return caracteres[numero - 10];
-}
-
-int getNumero(char letra)
-{
-  int indice;
-  for(indice = 0; indice < 26; indice++)
-  {
-    if(caracteres[indice] == letra)
-      return indice + 10;
-  }
-}*/
-
 double algumaParaDezAntes(char* numero, int base, int tamanho)
 {
   double ret = 0;
@@ -63,7 +47,7 @@ double algumaParaDezAntes(char* numero, int base, int tamanho)
     {
       
 
-      ret = ret + (double) ((numero[indice]-55) * pow(base, tamanho - indice-1));
+      ret = ret + (double) (toupper(numero[indice])-55) * pow(base, tamanho - indice-1);
     }
 
   }
@@ -84,14 +68,70 @@ double algumaParaDezDepois(char* numero, int base, int tamanho)
     }
     else
     {
-      ret = ret + (double) ((numero[indice]-55) * (1 / pow(base, indice + 1)));
+      ret = ret + (double) (toupper(numero[indice])-55) * (1 / pow(base, indice + 1));
        int a =0;
     }
   }
   return ret;
 }
 
-double algumaParaDez(char *numeroAntes,char *numeroDepois, int baseInicial,int tamanhoAntes,int tamanhoDepois)
+char* dezParaOutraAntes(int antesVirgula,int base)
+{
+
+int indice=0;
+char* conversaoInvertida = malloc(sizeof(char));
+
+while((antesVirgula % base)!=0)
+{
+conversaoInvertida[indice] = (char)antesVirgula%base + 48;
+antesVirgula=antesVirgula/base;
+indice++;
+realloc(conversaoInvertida,sizeof(char)+indice);
+}
+
+
+int tamanho = strlen(conversaoInvertida);
+char* conversao= malloc(sizeof(char)*tamanho);
+
+int i;
+for(i=0;i<tamanho;i++)
+{
+conversao[tamanho-i-1] =conversaoInvertida[i];
+
+}
+free(conversaoInvertida);
+
+return conversao;
+
+
+
+}
+
+char* dezParaOutraDepois(double depoisVirgula,int base)
+{
+  char*numero = malloc(sizeof(char));
+  int indice=0;
+  int digito=0;
+  
+  while(depoisVirgula!=0)
+  {
+    depoisVirgula*=base;
+
+    digito = (int) depoisVirgula;
+
+    
+    numero[indice] = (char)digito+48;
+    depoisVirgula-=digito;
+    indice++;
+    realloc(numero,sizeof(char)+indice);
+    
+   
+  }
+  int a=0;
+return numero;
+}
+
+char* algumaParaDez(char *numeroAntes,char *numeroDepois, int baseInicial,int tamanhoAntes,int tamanhoDepois,int baseFinal)
 {
   
 
@@ -99,10 +139,42 @@ double algumaParaDez(char *numeroAntes,char *numeroDepois, int baseInicial,int t
 
   double depois = algumaParaDezDepois(numeroDepois, baseInicial, tamanhoDepois);
 
-  double n = antes+depois;
   
-  return n;
+ char* b = dezParaOutraDepois(depois,baseFinal);
+
+ char* a = dezParaOutraAntes(antes,baseFinal);
+char* c;
+
+  strcat(a,".");
+  strcat(a,b);
+  
+  strcat(c,a);
+  free(a);
+  free(b);
+
+  
+  return c;
 }
+
+int verificar(char *numero, int tamanho, int base)
+{
+  int indice;
+  for(indice = 0; indice < tamanho; indice++)
+  {
+    if(!isdigit(numero[indice]))
+    {
+      if((toupper(numero[indice])-55) > base - 1)
+        return -1;
+      }
+    else
+    {
+      if((numero[indice]-48) > base - 1)
+        return -1;
+    }
+  }
+  return 1;
+}
+
 
 
 //double dezParaOutra(double numero, int baseFinal)
@@ -135,8 +207,13 @@ int main()
 
   int tamanhoDepois = strlen(depoisVirgula);
 
-  double a = algumaParaDez(antesVirgula,depoisVirgula,baseInicial,tamanhoAntes,tamanhoDepois);
-  printf("%lf",a);
+  int a = verificar(antesVirgula, tamanhoAntes, baseInicial);
+  int b = verificar(depoisVirgula, tamanhoDepois, baseInicial);
+  printf("%i", a);
+  printf("%i", b);
+
+  //char* a = algumaParaDez(antesVirgula,depoisVirgula,baseInicial,tamanhoAntes,tamanhoDepois,baseFinal);
+  //printf("\n%s",a);
 
   //printf("\nO resultado é: %s", numeroFinal);
 
